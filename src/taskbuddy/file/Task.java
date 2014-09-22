25cd5b2509fe2e50fdbf99e01aa34d0376d84564
sohyongsheng <sohyongsheng@gmail.com>
2014-09-22 21:07:19 +0800

    Implement deadline functionality of Tasks
    Issue #63 - https://github.com/cs2103aug2014-f11-3j/main/issues/63
    
    Why is this change necessary?
    Users often need to key in deadlines about their tasks (not just the
    task description). A task deadline can be expressed as a date and time.
    Therefore, it is useful to associate dates and times with tasks.
    
    How does it address the issue?
    Having Date and Time fields in the Task object allows for:
    - Easy access of date and time of the task deadline through getters
    - Easy manipulation and storage of date and time of task deadline
      through setters
    - Enforces the idea that each task can only have one deadline, i.e. one
      Date field and one Time field.
    
    What side effects does this change have?
    Everything:
    - UserInterface can now parse and add deadlines (in terms of date and
      time) to Logic.
    - Logic can manipulate task deadlines and pass them to Database for
      storage.
    - Storage can now have a good structure to store deadlines within
      (literally) tasks.
    - Most importantly, GoogleCalendarManager can add tasks to Google
      Calendar with the relevant deadlines.
    
    Task class implementation taken to be complete for now.
    Close #63

--- src/taskbuddy/file/Task.java
+++ src/taskbuddy/file/Task.java
@@ -8,12 +8,17 @@ package taskbuddy.file;
  *
  */
 public class Task {
+    // task description
     String description;
+
+    // date and time describes the task's deadline
+    Date date;
+    Time time;
 
     /**
      * Constructor for a <code>Task</code>, which initialises the task
-     * description. Initalisation of task description in here emphasies the fact
-     * that you cannot have a task without its description.
+     * description. Initalisation of task description in here emphasises the
+     * fact that you cannot have a task without its description.
      * 
      * @param inputDescription
      *            task description
@@ -40,4 +45,47 @@ public class Task {
     public void setDescription(String inputDescription) {
         this.description = inputDescription;
     }
+
+    /**
+     * Retrieves and returns the date of the task deadline as a
+     * <code>Date</code> object.
+     * 
+     * @return date of task deadline to retrieve
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * Takes in a <code>Date</code> object and sets it as the date of the task
+     * deadline
+     * 
+     * @param date
+     *            date of task deadline to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    /**
+     * Retrieves and returns the time of the task deadline as a
+     * <code>Time</code>object.
+     * 
+     * @return time of task deadline to retrieve
+     */
+    public Time getTime() {
+        return time;
+    }
+
+    /**
+     * Takes in a <code>Time</code> object and sets it as the time of the task
+     * deadline
+     * 
+     * @param time
+     *            time of task deadline to set
+     */
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
 }
--- test/taskbuddy/file/TaskTest.java
+++ test/taskbuddy/file/TaskTest.java
@@ -6,12 +6,17 @@ import org.junit.Test;
 
 public class TaskTest {
 
-    private String description;
-    private Task testTask;
+    Task testTask;
+
+    String description;
+    Date date;
+    Time time;
 
     public void setup() {
         description = "a description";
         testTask = new Task(description);
+        date = new Date(31, 12, 2014);
+        time = new Time(23, 59);
     }
 
     @Test
@@ -26,5 +31,18 @@ public class TaskTest {
         String description = "another description";
         testTask.setDescription(description);
         assertEquals(description, testTask.getDescription());
+    }
+
+    @Test
+    public void testSetDateAndTime() throws Exception {
+        setup();
+        
+        testTask.setDate(date);
+        assertTrue("date field of task is not set properly", testTask.getDate()
+                .equals(date));
+        
+        testTask.setTime(time);
+        assertTrue("time field of task is not set properly", testTask.getTime()
+                .equals(time));
     }
 }
