16770998154f1c7278b52b1aa68cb1355ddf84ec
Andrew Low <andrewjrlow@gmail.com>
2014-09-23 18:03:58 +0800

    Task and CommandParser classes added
    -

--- src/taskbuddy/file/Task.java
+++ src/taskbuddy/file/Task.java
@@ -7,85 +7,102 @@ package taskbuddy.file;
  * @author Soh Yong Sheng
  *
  */
-public class Task {
-    // task description
-    String description;
+import java.util.ArrayList;
+import java.util.Calendar;
 
-    // date and time describes the task's deadline
-    Date date;
-    Time time;
 
-    /**
-     * Constructor for a <code>Task</code>, which initialises the task
-     * description. Initalisation of task description in here emphasises the
-     * fact that you cannot have a task without its description.
-     * 
-     * @param inputDescription
-     *            task description
-     */
-    public Task(String inputDescription) {
-        this.description = inputDescription;
-    }
-
-    /**
-     * Returns the task description as a string
-     * 
-     * @return the task description as a string
-     */
-    public String getDescription() {
-        return description;
-    }
-
-    /**
-     * Sets the description of the task.
-     * 
-     * @param inputDescription
-     *            the task description
-     */
-    public void setDescription(String inputDescription) {
-        this.description = inputDescription;
-    }
-
-    /**
-     * Retrieves and returns the date of the task deadline as a
-     * <code>Date</code> object.
-     * 
-     * @return date of task deadline to retrieve
-     */
-    public Date getDate() {
-        return date;
-    }
-
-    /**
-     * Takes in a <code>Date</code> object and sets it as the date of the task
-     * deadline
-     * 
-     * @param date
-     *            date of task deadline to set
-     */
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    /**
-     * Retrieves and returns the time of the task deadline as a
-     * <code>Time</code>object.
-     * 
-     * @return time of task deadline to retrieve
-     */
-    public Time getTime() {
-        return time;
-    }
-
-    /**
-     * Takes in a <code>Time</code> object and sets it as the time of the task
-     * deadline
-     * 
-     * @param time
-     *            time of task deadline to set
-     */
-    public void setTime(Time time) {
-        this.time = time;
-    }
-
+class Task {
+	private String __title;
+	private String __description;
+	private Calendar __startTime;
+	private Calendar __endTime;
+	private int __priorityFlag;
+	private boolean __completionFlag;
+	private boolean __floatingTask;
+	
+	//basic constructors
+	public Task(){}
+	
+	public Task(String title){
+		this.__title = title;
+		this.__completionFlag = false;
+	}
+	
+	//basic accessors
+	public String getTitle(){
+		return this.__title;
+	}
+	
+	public String getDescription(){
+		return this.__description;
+	}
+	
+	public Calendar getStartTime(){
+		return this.__startTime;
+	}
+	
+	public Calendar getEndTime(){
+		return this.__endTime;
+	}
+	
+	public int getPriority(){
+		return this.__priorityFlag;
+	}
+	
+	public boolean getCompletionStatus(){
+		return this.__completionFlag;
+	}
+	
+	public boolean isFloatingTask(){
+		return this.__floatingTask;
+	}
+	
+	//basic mutators
+	public void setTitle(String nextTitle){
+		this.__title = nextTitle;
+	}
+	
+	public void setDescription(String nextDescription){
+		this.__description = nextDescription;
+	}
+	
+	public void setStartTime(Calendar nextStart){
+		this.__startTime = nextStart;
+	}
+	
+	public void setEndTime(Calendar nextEnd){
+		this.__endTime = nextEnd;
+	}
+	
+	public void setPriority(int nextPriority){
+		this.__priorityFlag = nextPriority;
+	}
+	
+	public void setCompletion(boolean nextStatus){
+		this.__completionFlag = nextStatus;
+	}
+	
+	public void setFloating(boolean nextFloatStat){
+		this.__floatingTask = nextFloatStat;
+	}
+	
+	public void checkFloating(Task task){
+		if (task.getEndTime() == null){
+			task.setFloating(true);
+		} else {
+			task.setFloating(false);
+		}
+	}
+	
+	//other class methods
+	public ArrayList<String> getTaskInfo(){
+		ArrayList<String> toDisplay = new ArrayList<String>();
+		toDisplay.add(__title);
+		toDisplay.add(__description);
+		toDisplay.add(__startTime.toString());
+		toDisplay.add(__endTime.toString());
+		toDisplay.add(Integer.toString(__priorityFlag));
+		toDisplay.add(Boolean.toString(__completionFlag));
+		return toDisplay;
+	}
 }
