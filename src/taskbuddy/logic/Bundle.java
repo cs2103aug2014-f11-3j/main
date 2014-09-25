bbe593cf24138bc4ef944409c5d8edcf0bea2482
Andrew Low <andrewjrlow@gmail.com>
2014-09-26 01:18:19 +0800

    created bundle

--- src/taskbuddy/logic/Bundle.java
+++ src/taskbuddy/logic/Bundle.java
@@ -1,101 +1,27 @@
 package taskbuddy.logic;
 
 import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.HashMap;
 
-class Task {
-	private String __title;
-	private String __description;
-	private Calendar __startTime;
-	private Calendar __endTime;
-	private int __priorityFlag;
-	private boolean __completionFlag;
-	private boolean __floatingTask;
+//Author: andrew
+
+class Bundle{
+	HashMap<String, Object> bundle;
 	
-	//basic constructors
-	public Task(){}
-	
-	public Task(String title){
-		this.__title = title;
-		this.__completionFlag = false;
+	//e.g bundle.putString("Failure", "Failed to load etc");
+	public void putString(String key,String s){
+		this.bundle.put(key, s);
 	}
 	
-	//basic accessors
-	public String getTitle(){
-		return this.__title;
+	public void putObject(String key, Object o){
+		this.bundle.put(key, o);
 	}
 	
-	public String getDescription(){
-		return this.__description;
+	public Object getItem(String key){
+		return this.bundle.get(key);
 	}
 	
-	public Calendar getStartTime(){
-		return this.__startTime;
-	}
-	
-	public Calendar getEndTime(){
-		return this.__endTime;
-	}
-	
-	public int getPriority(){
-		return this.__priorityFlag;
-	}
-	
-	public boolean getCompletionStatus(){
-		return this.__completionFlag;
-	}
-	
-	public boolean isFloatingTask(){
-		return this.__floatingTask;
-	}
-	
-	//basic mutators
-	public void setTitle(String nextTitle){
-		this.__title = nextTitle;
-	}
-	
-	public void setDescription(String nextDescription){
-		this.__description = nextDescription;
-	}
-	
-	public void setStartTime(Calendar nextStart){
-		this.__startTime = nextStart;
-	}
-	
-	public void setEndTime(Calendar nextEnd){
-		this.__endTime = nextEnd;
-	}
-	
-	public void setPriority(int nextPriority){
-		this.__priorityFlag = nextPriority;
-	}
-	
-	public void setCompletion(boolean nextStatus){
-		this.__completionFlag = nextStatus;
-	}
-	
-	public void setFloating(boolean nextFloatStat){
-		this.__floatingTask = nextFloatStat;
-	}
-	
-	public void checkFloating(Task task){
-		if (task.getEndTime() == null){
-			task.setFloating(true);
-		} else {
-			task.setFloating(false);
-		}
-	}
-	
-	//other class methods
-	public ArrayList<String> getTaskInfo(){
-		ArrayList<String> toDisplay = new ArrayList<String>();
-		toDisplay.add(__title);
-		toDisplay.add(__description);
-		toDisplay.add(__startTime.toString());
-		toDisplay.add(__endTime.toString());
-		toDisplay.add(Integer.toString(__priorityFlag));
-		toDisplay.add(Boolean.toString(__completionFlag));
-		return toDisplay;
+	public String toString(){
+		return this.bundle.toString();
 	}
 }
-
--- src/taskbuddy/logic/CommandParser.java
+++ src/taskbuddy/logic/CommandParser.java
@@ -1,5 +1,85 @@
 package taskbuddy.logic;
+import java.util.ArrayList;
+import java.util.Stack;
 
+
+//Author: andrew
 public class CommandParser {
 
+	private Stack<ArrayList<String>> commandStack = new Stack<ArrayList<String>>();
+	private Stack<ArrayList<String>> redoStack = new Stack<ArrayList<String>>();
+	// if new command is parsed, clear redo stack;
+
+	static enum Command {
+		CREATE(1), READ(2), UDPATE(3), DELETE(4), UNDO(5), REDO(6);
+
+		private int code;
+
+		private Command(int c) {
+			code = c;
+		}
+
+		public int getCode() {
+			return code;
+		}
+
+		@Override
+		public String toString() {
+			String s = super.toString();
+			return s.substring(0, 1) + s.substring(1).toLowerCase();
+		}
+	}
+
+	public enum Acknowledge {
+
+	}
+
+	public Task createTask() {
+		Task task = new Task();
+
+		return task;
+	}
+
+	public void putTask(Task task) {
+		//Database.addTask(task);
+	}
+	
+	public void readTask(String title){
+		
+	}
+	
+	public void deleteTask(String title){
+		
+	}
+
+	public void userInputs(ArrayList<String> userIn) {
+		String commandType = userIn.get(0);
+		if (commandType.equalsIgnoreCase("add")) {
+			redoStack = new Stack<ArrayList<String>>();
+			commandStack.add(userIn);
+			String desc = userIn.get(1);
+			String endDate = userIn.get(2);
+			String endTime = userIn.get(3);
+			String title = userIn.get(4);
+			
+		}
+		if (commandType.equalsIgnoreCase("display")) {
+			String title = userIn.get(4);
+			//Task newTask = readTask(title);
+		}
+		if (commandType.equalsIgnoreCase("edit")) {
+			redoStack = new Stack<ArrayList<String>>();
+			commandStack.add(userIn);
+		}
+		if (commandType.equalsIgnoreCase("delete")) {
+			redoStack = new Stack<ArrayList<String>>();
+			commandStack.add(userIn);
+		}
+		if (commandType.equalsIgnoreCase("undo")) {
+
+		}
+		if (commandType.equalsIgnoreCase("redo")) {
+			
+		}
+	}
 }
