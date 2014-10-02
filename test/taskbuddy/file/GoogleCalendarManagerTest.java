10065c94d2f3764b39a9ab95c3dc10f03a6fd4b4
ipeech <i.peech@gmail.com>
2014-10-02 21:25:00 +0800

    Added unit test: testDisplatTaskTime
    This test is important as it ensures that the time of the task passed into
    GoogleCalendarManager from Database is read correctly by
    GoogleCalendarManager, before GoogleCalendarManager adds this task onto
    the Google Calendar online servers.

--- test/taskbuddy/file/GoogleCalendarManagerTest.java
+++ test/taskbuddy/file/GoogleCalendarManagerTest.java
@@ -16,6 +16,7 @@ public class GoogleCalendarManagerTest {
 	GoogleCalendarManager gooCal = new GoogleCalendarManager();
 	Task task = new Task ("Task Description");
 	Date date = new Date (2,10,2014);
+	Time time = new Time (21,30);
 	
 	
 	// This tests the ability to read the description of a task sent to it  
@@ -26,8 +27,17 @@ public class GoogleCalendarManagerTest {
 	
 	// This tests the ability to read the date of a task sent to it  
 	@Test
-	public void testDisplayDate() {
+	public void testDisplayTaskDate() {
 		task.setDate(date);
 		assertEquals("If this test fails, the task date is not reading properly.", "2.10.2014", gooCal.displayTaskDate(task));
 	}
+	
+	// This tests the ability to read the time of a task sent to it  
+	@Test
+	public void testDisplayTaskTime() {
+		task.setTime(time);
+		assertEquals("If this test fails, the task time is not reading properly.", "21.30", gooCal.displayTaskTime(task));
+	}
+	
+	
 }
