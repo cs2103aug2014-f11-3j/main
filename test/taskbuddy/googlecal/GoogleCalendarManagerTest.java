647a68258aef8ff08fc96abfc1559312e87be521
ipeech <i.peech@gmail.com>
2014-10-06 23:39:35 +0800

    Added JUnit test case for isUserOnline() method
    This test case ensures the isUserOnline() method in GoogleCalendarManager
    works as expected.

--- test/taskbuddy/googlecal/GoogleCalendarManagerTest.java
+++ test/taskbuddy/googlecal/GoogleCalendarManagerTest.java
@@ -2,6 +2,8 @@ package taskbuddy.googlecal;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 import org.junit.Before;
@@ -26,9 +28,19 @@ public class GoogleCalendarManagerTest {
 	}
 	
 	// This test tests the ability for GoogleCalendarManager to successfully add an event to Google Calendar servers.
+	/*
 	@Test
 	public void testAdd() {
 		assertTrue("Failed because event was not successfully added to Google Calendar Server", gooCal.add(task));		
 	}
+	*/
+	
+	
+	// This test tests the ability for GoogleCalendarManager to detect the online status of the user. 
+	@Test
+	public void testIsUserOnline()  throws UnknownHostException, IOException  {
+			assertTrue("Failed because of the inability to detect correct online status", gooCal.isUserOnline());
+	}
+
 	
 }
