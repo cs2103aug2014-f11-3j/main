7f5e003914127076cb849849aeb2264d3335d9f8
Soh Yong Sheng <sohyongsheng@gmail.com>
2014-10-10 00:59:12 +0800

    Implement bundle standard

--- test/taskbuddy/database/AllTests.java
+++ test/taskbuddy/database/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TaskLogger.class, DatabaseTest.class, DbCommandTest.class })
+@SuiteClasses({ TaskLoggerTest.class, DatabaseTest.class, DbCommandTest.class })
 public class AllTests {
 
 }
