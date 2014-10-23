c5dfad522e94e30b0de61467f7b0a38039033794
Andrew Low <andrewjrlow@gmail.com>
2014-10-23 17:39:07 +0800

    added stack trace

--- test/taskbuddy/logic/LogicDatabaseIntegrate.java
+++ test/taskbuddy/logic/LogicDatabaseIntegrate.java
@@ -15,33 +15,40 @@ public class LogicDatabaseIntegrate {
 
 	private static String nullValue = "padding value";
 	private CommandParser cp;
-	
+
 	@Test
-	public void testAddFloating() throws ParseException, IOException{
-		cp = new CommandParser();
-		UserInputBundle uf = new UserInputBundle();
-		AcknowledgeBundle bf = new AcknowledgeBundle();
-		uf.putTitle("task floating");
-		uf.putDescription("test task for integration");
-		uf.putEndDate(nullValue);
-		uf.putEndTime(nullValue);
-		uf.putStartDate(nullValue);
-		uf.putStartTime(nullValue);
-		uf.putCommand("add");
-		cp.parseUserInputs(uf);
-		Database db = cp.getDatabase();
-		db.printTasks();
-		String status = bf.getStatus();
-		assertEquals("Success", status);
-	}
-	/*
-	@After
-	public void deleteLog(){
-		File log = new File("log");
-		if (log.isFile()){
-			log.delete();
+	public void testAddFloating() throws ParseException, IOException {
+		try {
+			cp = new CommandParser();
+			UserInputBundle uf = new UserInputBundle();
+			AcknowledgeBundle bf = new AcknowledgeBundle();
+			uf.putTitle("task floating");
+			uf.putDescription("test task for integration");
+			uf.putEndDate(nullValue);
+			uf.putEndTime(nullValue);
+			uf.putStartDate(nullValue);
+			uf.putStartTime(nullValue);
+			uf.putCommand("add");
+			pr(uf.getCommand());
+			pr(uf.getTitle());
+			pr(uf.getDescription());
+			cp.parseUserInputs(uf);
+			Database db = cp.getDatabase();
+			db.printTasks();
+			String status = bf.getStatus();
+			assertEquals("Success", status);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
-	*/
-	
+
+	public void pr(String s) {
+		System.out.println(s);
+	}
+
+	/*
+	 * @After public void deleteLog(){ File log = new File("log"); if
+	 * (log.isFile()){ log.delete(); } }
+	 */
+
 }
