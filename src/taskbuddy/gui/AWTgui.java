31525405c6f9e857e0ff4ff73899e10fcc4bff1e
ipeech <i.peech@gmail.com>
2014-10-29 04:09:23 +0800

    GUI: Change way of displaying, clear commandTextField upon send
    - Displaying mode has been altered. Removed append.
    - Add feature to automtically scroll to the end of the displayTextArea by
      moving the caret to the position of the string length. (scroll position)
    - Each time a user sends a command, the command text box is automatically
      cleared, ready for the next command.

--- src/taskbuddy/gui/AWTgui.java
+++ src/taskbuddy/gui/AWTgui.java
@@ -19,6 +19,7 @@ import java.text.ParseException;
 
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
+import javax.swing.text.DefaultCaret;
 
 import taskbuddy.googlecal.GooCalBackend;
 import taskbuddy.logic.CommandParser;
@@ -84,6 +85,7 @@ public class AWTgui extends Frame implements ActionListener, WindowListener{
 		add(displayLabel);      
 		displayTextArea = new TextArea(stringResponse, 30, 50, 1);
 		displayTextArea.setEditable(false);
+		
 		add(displayTextArea);
 		
 		//BUTTON
@@ -194,8 +196,14 @@ public class AWTgui extends Frame implements ActionListener, WindowListener{
 	
 	public static void appendToDisplay(String toAppend) {
 		display = display + System.getProperty("line.separator") + toAppend + System.getProperty("line.separator");
+	}
 	
-		
+	public static void clearDisplayString() {
+		display = "";
+	}
+	
+	public void clearCommandTextField() {
+		commandTextField.setText("");
 	}
 	
 	public static void setResponseString(String response) {
@@ -231,9 +239,16 @@ public class AWTgui extends Frame implements ActionListener, WindowListener{
 //			// TODO Auto-generated catch block
 //			e.printStackTrace();
 //		}
+		
+		DisplayObserver displayObserver = new DisplayObserver();
+		displayObserver.generateDisplayString();
 //		
 		responseTextField.setText(stringResponse);
-		displayTextArea.append(display);
+		//displayTextArea.append(display);
+		displayTextArea.setText(display);
+		displayTextArea.setCaretPosition(display.length()); // Used to move cart (scrolling) of displayTextArea to the bottom
+		clearDisplayString();
+		clearCommandTextField();
 		
 		
 		//Integer.parseInt(tfInput.getText());
