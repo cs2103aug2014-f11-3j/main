4e508791c848103e3c80fa589a900a11d5a27c82
Andrew Low <andrewjrlow@gmail.com>
2014-10-30 22:28:49 +0800

    changed edit method
    can edit date and time independent of each other now

--- src/taskbuddy/logic/EditCommand.java
+++ src/taskbuddy/logic/EditCommand.java
@@ -1,54 +1,77 @@
 package taskbuddy.logic;
 
+import java.util.Calendar;
+
 import taskbuddy.database.Database;
 
 public class EditCommand {
-	
+
 	private static String nullValue = "padding value";
-	
-	public static AcknowledgeBundle editTask(UserInputBundle extras, Database db){
+
+	public static AcknowledgeBundle editTask(UserInputBundle extras, Database db) {
 		AcknowledgeBundle ack = new AcknowledgeBundle();
 		Task toEdit = new Task();
-		int taskID =  Integer.parseInt(extras.getTaskID());
-		try{
+		int taskID = Integer.parseInt(extras.getTaskID());
+		try {
 			toEdit = db.read(taskID);
 			ack.putOldTask(toEdit);
 			String title = extras.getTitle();
-			if (!title.equals(nullValue)){
+			if (!title.equals(nullValue)) {
 				toEdit.setTitle(title);
 			}
-			
+
 			String desc = extras.getDescription();
-			if (!desc.equals(nullValue)){
+			if (!desc.equals(nullValue)) {
 				toEdit.setDescription(desc);
 			}
-			
+
 			String startDate = extras.getStartDate();
-			if (!startDate.equals(nullValue)){
-				String startTime = extras.getStartTime();
-				if (!startTime.equals(nullValue)){
-					toEdit.setStartTime(startDate, startTime);
-				} else {
-					startTime = "0000";
-					toEdit.setStartTime(startDate, startTime);
-				}
+			String startTime = extras.getStartTime();
+			if (!startDate.equals(nullValue) && !startTime.equals(nullValue)) {
+				toEdit.setStartTime(startDate, startTime);
+			} else if (!startDate.equals(nullValue)) {
+				Calendar cal = Calendar.getInstance();
+				String[] ddmmyyyy = startDate.split("/");
+				int d = Integer.parseInt(ddmmyyyy[0]);
+				int m = Integer.parseInt(ddmmyyyy[1]);
+				int y = Integer.parseInt(ddmmyyyy[2]);
+				cal.set(y, m, d);
+				toEdit.setStartTime(cal);
+			} else if (!startTime.equals(nullValue)) {
+				Calendar cal = Calendar.getInstance();
+				int h = Integer.parseInt(startTime.substring(0, 2));
+				int m = Integer.parseInt(startTime.substring(2));
+				cal.set(Calendar.HOUR_OF_DAY, h);
+				cal.set(Calendar.MINUTE, m);
+				toEdit.setStartTime(cal);
 			}
-			
+
 			String endDate = extras.getEndDate();
-			if (!endDate.equals(nullValue)){
-				String endTime = extras.getEndTime();
-				if (!endTime.equals(nullValue)){
-					toEdit.setStartTime(endDate, endTime);
-				} else {
-					endTime = "2359";
-					toEdit.setStartTime(endDate, endTime);
-				}
+			String endTime = extras.getEndTime();
+			if (!endDate.equals(nullValue) && !endTime.equals(nullValue)) {
+				toEdit.setStartTime(endDate, endTime);
+			} else if (!endDate.equals(nullValue)) {
+				Calendar cal = Calendar.getInstance();
+				String[] ddmmyyyy = endDate.split("/");
+				int d = Integer.parseInt(ddmmyyyy[0]);
+				int m = Integer.parseInt(ddmmyyyy[1]);
+				int y = Integer.parseInt(ddmmyyyy[2]);
+				cal.set(y, m, d);
+				toEdit.setStartTime(cal);
+			} else if (!endTime.equals(nullValue)) {
+				Calendar cal = Calendar.getInstance();
+				int h = Integer.parseInt(endTime.substring(0, 2));
+				int m = Integer.parseInt(endTime.substring(2));
+				cal.set(Calendar.HOUR_OF_DAY, h);
+				cal.set(Calendar.MINUTE, m);
+				toEdit.setStartTime(cal);
 			}
+
 			db.edit(toEdit);
 			ack.putSuccess();
 			ack.putMessage("task edited");
 			ack.putTask(toEdit);
-		} catch (Exception e){
+		} catch (Exception e) {
 			ack.putFailure();
 			ack.putMessage("Unable to find task in DB");
 		}
