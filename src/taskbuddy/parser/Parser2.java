7d47cfe84b28627b07ad7e9eac814154c728a941
Andrew Low <andrewjrlow@gmail.com>
2014-11-08 16:28:57 +0800

    pushing to prevent errors

---
+++ src/taskbuddy/parser/Parser2.java
@@ -0,0 +1,16 @@
+package taskbuddy.parser;
+
+import taskbuddy.logic.UserInputBundle;
+
+
+//@Author: andrew
+public class Parser2 {
+
+	public void parseOtherCommands(String userInputLine){
+		UserInputBundle inputs = new UserInputBundle();
+		
+	}
+	
+	
+	
+}
