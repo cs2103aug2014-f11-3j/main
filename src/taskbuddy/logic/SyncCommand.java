aaf272c44d3ad2703bec72a013874126e48167e8
Andrew Low <andrewjrlow@gmail.com>
2014-11-10 22:35:21 +0800

    sync command pushed

--- src/taskbuddy/logic/SyncCommand.java
+++ src/taskbuddy/logic/SyncCommand.java
@@ -3,15 +3,18 @@
 
 package taskbuddy.logic;
 
+import java.io.IOException;
+import java.text.ParseException;
+
 import taskbuddy.database.Database;
 
 public class SyncCommand {
 
-	public static AcknowledgeBundle SyncCalendars(){
+	public static AcknowledgeBundle SyncCalendars() throws ParseException, IOException{
 		AcknowledgeBundle acks = new AcknowledgeBundle();
 		CommandParser cp = CommandParser.getInstance();
 		Database db = cp.getDatabase();
-		
+		db.sync();
 		return acks;
 	}
 	
